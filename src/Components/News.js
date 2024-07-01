cb6c06caf9bb143ed10630666a5eadafce168c8b
Harshvardhan <107619247+ftharsh@users.noreply.github.com>
2024-07-01 08:37:47 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,7 +7,7 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News =(props)=> {
     const[articles,setArticles]=useState([])
-    const[loading,setLoading]=useState(true)
+    const[loading,setLoading]=useState(true) 
     const[page,setPage]=useState(1)
     const[totalResults,setTotalResults]=useState(0)
     const capitaliz = (string) => {
